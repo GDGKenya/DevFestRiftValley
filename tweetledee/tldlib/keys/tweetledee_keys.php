6c072f89cc5255bcd8e3885d5e1e5d6fa78698af
Maye Edwin <mayedwine1@gmail.com>
2017-09-11 13:43:06 +0300

    Commit

---
+++ tweetledee/tldlib/keys/tweetledee_keys.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+* PLEASE READ THE FOLLOWING
+* Enter your Twitter application information in the appropriate lines below this comment section.
+* Please leave the single quotes around the alphanumeric strings that you enter.
+*
+*  An appropriately formatted keys file should look like this:
+*  	$my_consumer_key        = 'CoMpLetEly8oGusK3y';
+*	$my_consumer_secret     = 'SuPercaliFragilist1cEXPiaLidOCIous';
+*	$my_access_token        = 'AcC355MePLz';
+*	$my_access_token_secret = '5555shTh1s1iS53cr3t';
+*	$my_domain              = 'http://www.toolegittoquit.com';
+*
+*   Please note that the number of characters in the above examples are not necessarily correct and that these are bogus tokens.
+*   Users who have attempted a 'workaround' for their API limits received tweets from Mary Poppins and MC Hammer...
+*
+*  OK, now it's your turn.  I used ALL CAPS where you should insert your information.  If it's not in ALL CAPS, it's a good bet
+*  that you should not modify it.
+*
+*  Enter your information below this line:
+*/
+
+$my_consumer_key        = 'pPGFdJaLlQ4pBHy950xFvXm2r';
+$my_consumer_secret     = 'P0DsymDnTWawa2VKtjnVimj0IxDhVYnCOKAuGRtbfoq2fd0DSn';
+$my_access_token        = '2457971843-T2sTKOStGFrVHiY6dF6KktCTDB0NqRRGqpVK5fS';
+$my_access_token_secret = '8TQpkap5GighFYXZhfvF8b26EU3tzYc2RDplvJL0mYbcw';
+$my_domain              = 'https://devfestriftvalley.firebaseapp.com/';
+
+
+?>
